2e18765b1dcba558d3751ac24ee64d667627ca39
Scoot_Kampfer <gabenoah8@gmail.com>
2025-04-17 12:24:54 -0400

    added end scene skeleton

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 import Titre from './titre.js'
 import Game from './game.js'
 import Menu from "./menu.js"
+import End from "./end.js"
 
 const titre_scene = new Titre();
 const game_scene = new Game();
 const menu_scene = new Menu();
+const end_scene = new End();
 
 var config = {
     type: Phaser.AUTO,
@@ -24,5 +26,6 @@ var game = new Phaser.Game(config);
 game.scene.add('titre_scene', titre_scene);
 game.scene.add('game_scene', game_scene);
 game.scene.add("menu_scene", menu_scene);
+game.scene.add("end_scene", end_scene);
 
 game.scene.start('titre_scene');
